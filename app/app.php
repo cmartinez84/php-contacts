5b99dd1b5690f556b2a122d264e815ee9301913b
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-09 09:52:54 -0700

    add required and length attributes to input

--- app/app.php
+++ app/app.php
@@ -1,12 +1,12 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    // require_once __DIR__."/../src/Contact.php";
+    require_once __DIR__."/../src/Contact.php";
     date_default_timezone_set('America/Los_Angeles');
 
     session_start();
-    // if (empty($_SESSION['hangman'])){
-    //   $_SESSION['hangman'] = "";
-    // }
+    if (empty($_SESSION['list_of_contacts'])){
+      $_SESSION['list_of_contacts'] = array();
+    }
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -15,13 +15,17 @@
     $app->get("/", function() use ($app){
       return $app['twig']->render('home.html.twig');
     });
-    return $app;
+
     $app->post("/create_contact", function() use ($app){
+        $new_contact = new Contact($_POST['first_name'],$_POST['last_name'], $_POST['street'], $_POST['city'], $_POST['state'], $_POST['zip'], $_POST['image'] );
+        $new_contact-> save();
+        var_dump($new_contact);
       return $app['twig']->render('create_contact.html.twig');
     });
-    return $app;
+
     $app->post("/delete_contact", function() use ($app){
       return $app['twig']->render('delete_contact.html.twig');
     });
+
     return $app;
 ?>
