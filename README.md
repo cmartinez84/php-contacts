4cf73348d712bbd28e8996a3038f9618e2a90907
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-09 16:02:17 -0700

    update README.md

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
-user is presented with form to enter first name, last name, street, city, state, zip code with text inputs and avatar with radio buttons.
-user will press submit, and be taken to a confirmation page where the most recent content will be displayed on screen
-new content page has button to return to home page
-home page will display all contacts entered into form so far.
-the form is inifiltely reusable
-a button exists at the very bottom to delete all contacts
-when button is pressed, user is taken to a confirmation page to indicate they have cleared all contacts
-delete page has button to begin readding contacts again
+
+# _Address Book_
+
+#### _An address book app built for Epicodus, Sept 9, 2016_
+
+#### By _**Chris Martinez**_
+
+## Description
+
+_This app will create a PHP-based address book that will store user input for new contacts and demo a list of pre-made contacts. This app uses Silex and Twig, which will need to be obtained by running composer install after downloaded. This app uses cookies to store data, so please ensure cookies are enabled before running_
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open an HTTP server in the cloned directory_
+* _After navigating into folder, run composer install_
+* _To play locally, run local server out of web folder
+
+* _Link to repository: 'https://github.com/cmartinez84/php-contacts'_
+
+
+## Known Bugs
+
+_No known bugs have been found to date._
+
+## Support and contact details
+
+_Reach out to Chris Martinez, the creator, at cadamomclouds@yahoo.com, if any bugs are found_
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, JQuery, JavaScript, PHP, Twig, Silex_
+
+## Specifications
+
+| Behavior |
+| --- |
+| User is presented with form to submit contact Information. First and Last Name are required |
+| User hits submit, and contact info displays on "create_contact" page, formatted, with contact's initials on the left|
+|When user navigates to home page, new contact information is posted along with all previous entries|
+|User can hit delete button at any time and delete all entries stored. When clicked, user will be navigated to a separate page confirming they have deleted their content|
+|User can navigate to the home page at any time|
+|User can press a demo button at any time to quickly try out the app with three preset contacts|
+|After demo button is hit, the demo contacts will be stored until the user hits delete|
+|User can hit delete at any time and erase all contact|
+
+### License
+
+
+Copyright (c) 2016 **_MIT Licence_**
--- src/Contact.php
+++ src/Contact.php
@@ -1,111 +1,107 @@
 <?php
     class Contact
     {
-    private $first_name;
-    private $last_name;
-    private $street;
-    private $city;
-    private $state;
-    private $zip;
-    private $phone;
-    private $email;
-    private $initials;
-
-    function __construct($first_name, $last_name, $street, $city, $state, $zip, $phone, $email)
+        private $first_name;
+        private $last_name;
+        private $street;
+        private $city;
+        private $state;
+        private $zip;
+        private $phone;
+        private $email;
+        private $initials;
+        function __construct($first_name, $last_name, $street, $city, $state, $zip, $phone, $email)
+            {
+            $this->first_name = ucwords(strtolower($first_name));
+            $this->last_name = ucwords(strtolower($last_name));
+            $this->street = ucwords(strtolower($street));
+            $this->city = ucwords(strtolower($city));
+            $this->state = strtoupper($state);
+            $this->zip = $zip;
+            $this->phone = $phone;
+            $this->email = $email;
+            $this->intials = strtoupper(substr($this->first_name, 0, 1) . substr($this->last_name, 0, 1));
+            }
+        function getfirst_Name()
+            {
+            return $this->first_name;
+            }
+        function getlast_Name()
+            {
+            return $this->last_name;
+            }
+        function getStreet()
+            {
+            return $this->street;
+            }
+        function getCity()
+            {
+            return $this->city;
+            }
+        function getState()
         {
-        $this->first_name = ucwords(strtolower($first_name));
-        $this->last_name = ucwords(strtolower($last_name));
-        $this->street = ucwords(strtolower($street));
-        $this->city = ucwords(strtolower($city));
-        $this->state = strtoupper($state);
-        $this->zip = $zip;
-        $this->phone = $phone;
-        $this->email = $email;
-        $this->intials = strtoupper(substr($this->first_name, 0, 1) . substr($this->last_name, 0, 1));
+            return $this->state;
         }
-    function getfirst_Name()
+        function getZip()
         {
-        return $this->first_name;
+            return $this->zip;
         }
-    function getlast_Name()
+        function getEmail()
         {
-        return $this->last_name;
+            return $this->email;
         }
-    function getStreet()
+        function getPhone()
         {
-        return $this->street;
+            return $this->phone;
         }
-    function getCity()
+        function getInitials()
         {
-        return $this->city;
+            return $this->initials;
         }
-    function getState()
-    {
-        return $this->state;
-    }
-    function getZip()
-    {
-        return $this->zip;
-    }
-    function getEmail()
-    {
-        return $this->email;
-    }
-    function getPhone()
-    {
-        return $this->phone;
-    }
-    function getInitials()
-    {
-        return $this->initials;
-    }
-    function setFirst_Name($new_first_name)
-    {
-        $this->first_name = $first_name;
-    }
-    function setLast_Name($new_last_name)
-    {
-        $this->last_name = $new_last_name;
-    }
-    function setStreet($new_street)
-    {
-        $this->street = $new_street;
-    }
-    function setCity($new_city)
-    {
-        $this->city = $new_city;
-    }
-    function setState($new_state)
-    {
-        $this->state =$new_state;
-    }
-    function setZip($new_zip)
-    {
-        $this->zip = $new_zip;
-    }
-    function setEmail($new_email)
-    {
-        $this->email = $new_email;
-    }
-    function setPhone($new_phone)
-    {
-        $this->phone = $new_phone;
-    }
-
-    function save()
-    {
-        array_push($_SESSION["list_of_contacts"], $this);
-    }
-    static function getAll()
-    {
-        return $_SESSION['list_of_contacts'];
-     }
-    static function deleteAll()
-    {
-        $_SESSION['list_of_contacts'] = "";
-    }
-
-
+        function setFirst_Name($new_first_name)
+        {
+            $this->first_name = $first_name;
+        }
+        function setLast_Name($new_last_name)
+        {
+            $this->last_name = $new_last_name;
+        }
+        function setStreet($new_street)
+        {
+            $this->street = $new_street;
+        }
+        function setCity($new_city)
+        {
+            $this->city = $new_city;
+        }
+        function setState($new_state)
+        {
+            $this->state =$new_state;
+        }
+        function setZip($new_zip)
+        {
+            $this->zip = $new_zip;
+        }
+        function setEmail($new_email)
+        {
+            $this->email = $new_email;
+        }
+        function setPhone($new_phone)
+        {
+            $this->phone = $new_phone;
+        }
+        function save()
+        {
+            array_push($_SESSION["list_of_contacts"], $this);
+        }
+        static function getAll()
+        {
+            return $_SESSION['list_of_contacts'];
+         }
+        static function deleteAll()
+        {
+            $_SESSION['list_of_contacts'] = "";
+        }
     }
 
  ?>
