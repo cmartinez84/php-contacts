455d324c5376a1f0a8bac5c29dd11b57963015e2
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-09 09:22:19 -0700

    create getters for user input

--- src/Contact.php
+++ src/Contact.php
@@ -8,7 +8,55 @@
     private $state;
     private $zip;
     private $image;
-    function __construct($first_name, $last_name)
+    function __construct($first_name, $last_name, $street, $city, $state, $zip, $image)
+        {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->street = $street;
+        $this->city = $city;
+        $this->state = $state;
+        $this->zip = $zip;
+        $this->image = $image;
+        }
+    function getFirstName()
+        {
+        return $this->first_name;
+        }
+    function getLastName()
+        {
+        return $this->last_name;
+        }
+    function getStreet()
+        {
+        return $this->street;
+        }
+    function getCity()
+        {
+        return $this->city;
+        }
+    function getState()
+    {
+        return $this->state;
+    }
+    function getZip()
+    {
+        return $this->zip;
+    }
+    function save()
+    {
+        $_SESSION["list_of_contacts"] = $this;
+    }
+    static function getAll()
+    {
+        return $_SESSION['list_of_contacts'];
+     }
+    static function deleteAll()
+    {
+        $_SESSION['list_of_contacts'] = "";
+    }
+
+
+
     }
 
  ?>
