aedd746e77a01d1388cfbf8f68427ff1ecfee535
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-09 09:36:08 -0700

    add form to homepage

--- web/index.php
+++ web/index.php
@@ -5,6 +5,10 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="/css/bootstrap-theme.min.css" type="text/css">
+    <link rel="stylesheet" href="/css/mystyles.css" type="text/css">
     <meta charset="utf-8">
     <title></title>
   </head>
